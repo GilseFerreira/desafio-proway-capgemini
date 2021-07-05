2a2982b9658e4cae49e1da831083ef8fe8e33bec
Gilse Ferreira <gilse-af@hotmail.com>
2021-07-04 23:50:11 -0300

    Start do projeto

---
+++ gerenciamento-de-anuncios/src/test/java/com/capgemini/desafioproway/gerenciamentodeanuncios/GerenciamentoDeAnunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.desafioproway.gerenciamentodeanuncios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAnunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
