296eb957bf67627c0dafc6e3e06247ec39af798a
Gilse Ferreira <gilse-af@hotmail.com>
2021-07-04 23:52:25 -0300

    Start do projeto

---
+++ gerenciamento-de-anuncios/src/main/java/com/capgemini/desafioproway/gerenciamentodeanuncios/GerenciamentoDeAnunciosApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.desafioproway.gerenciamentodeanuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAnunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAnunciosApplication.class, args);
+	}
+
+}
